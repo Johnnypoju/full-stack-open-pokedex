899b37786af52b3f96e01808889038e3123921c1
Joonas <jonde@pop-os.localdomain>
2023-02-25 12:35:49 +0200

    adding http_checks and an interface for a healthcheck

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '.', 'dist', 'index.html'))
 })
